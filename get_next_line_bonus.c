ac766b6367f1b405e39b5eb14ab5c1c17e218323
YangGang <dev.ingyeong@gmail.com>
2023-05-27 15:37:18 +0900

    add gnl_bonus

---
+++ get_next_line_bonus.c
@@ -0,0 +1,125 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: inseok <inseok@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/23 15:49:51 by inseok            #+#    #+#             */
+/*   Updated: 2023/05/23 15:49:51 by inseok           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+t_list	*gnl_lstnew(int fd, t_list **head)
+{
+	t_list	*new;
+
+	new = (t_list *)malloc(sizeof(t_list));
+	if (!new)
+		return (0);
+	new->index = fd;
+	new->next = *head;
+	return (new);
+}
+
+t_list *find_node(int fd, t_list * head)
+{
+	t_list * p;
+	p = head;
+	while(p != NULL)
+	{
+		if(p->index == fd)
+			return(p);
+		p = head->next;
+	}
+	return (NULL);
+}
+
+void free_list(int fd, t_list ** head)
+{
+	t_list *p;
+	t_list *temp;
+
+	p = *head;
+	temp = find_node(fd, *head);
+	if(temp == *head)
+	{
+		*head = (*head)->next;
+		free(temp);
+
+	}
+	else{
+		while(p != NULL)
+		{
+			if(p == temp)
+			{
+				p->next = p->next->next;
+				free(temp);
+			}
+			p = p->next;
+		}
+	}
+	
+
+}
+
+char* free_line(char ** line)
+{
+	free(*line);
+	*line = NULL;
+	return(NULL);
+}
+
+char *read_buffer(char **line, int fd)
+{
+	char		*read_buffer;
+	int			read_size;
+	char		*temp;
+
+	read_buffer = (char *)malloc(sizeof(char) * BUFFER_SIZE);
+	if (!read_buffer)
+		return(free_line(line));
+
+	while (gnl_strchr(*line) < 0)
+	{
+		read_size = read(fd, read_buffer, BUFFER_SIZE);
+		if (read_size <= 0)
+			break ;
+		temp = gnl_strjoin(*line, read_buffer, read_size);
+		free(*line);
+		*line = temp;
+	}
+	free(read_buffer);
+	if(read_size < 0)
+		return(free_line(line));
+	return(*line);
+}
+
+char	*get_next_line(int fd)
+{
+	static t_list* head;
+	t_list		*p;
+	char * result;
+
+
+	if (fd < 0 || BUFFER_SIZE < 1)
+		return (NULL);
+	if (!(find_node(fd, head)))
+		if (!(gnl_lstnew(fd, &head)))
+			return (NULL);
+	
+	p = find_node(fd, head);
+	p->line = read_buffer(&(p->line), fd);
+	if(gnl_strchr(p->line) < 0)
+	{
+		result = p->line;
+		p->line = NULL;
+		return (result);
+	}
+	result = gnl_split(&(p->line), gnl_strchr(p->line));
+	if (!result)
+		return(free_line(&(p->line)));
+	return (result);
+}
---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,114 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: inseok <inseok@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/23 15:48:58 by inseok            #+#    #+#             */
+/*   Updated: 2023/05/23 15:48:58 by inseok           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+int	gnl_strchr(const char *s)
+{
+	int	i;
+
+	i = 0;
+	if (!s)
+		return (-2);
+	while (s[i])
+	{
+		if (s[i] == '\n')
+			return (i);
+		i++;
+	}
+	return (-1);
+}
+
+char	*gnl_strjoin(char const *s1, char const *s2, int s2_len)
+{
+	size_t	len;
+	char	*s3;
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	if (!s1 && !s2)
+		return (NULL);
+	else if (!s1)
+		return (gnl_strndup(s2, s2_len));
+	else if (!s2)
+		return (gnl_strndup(s1, gnl_strlen(s1)));
+	len = gnl_strlen((char *)s1) + s2_len;
+	s3 = (char *)malloc(sizeof(char) * (len + 1));
+	if (!s3)
+		return (NULL);
+	while (*s1)
+		s3[i++] = *s1++;
+	while (j < s2_len)
+		s3[i + j++] = *s2++;
+	s3[i+j] = '\0';
+	return (s3);
+}
+
+int	gnl_strlen(const char *s)
+{
+	int	i;
+
+	if (!s)
+		return (0);
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*gnl_strndup(const char *s, int n)
+{
+	char	*result;
+	int		i;
+
+	if (!s)
+		return (NULL);
+	result = (char *)malloc(sizeof(char) * (n + 1));
+	if (!result)
+		return (NULL);
+	i = 0;
+	while (i < n && s[i])
+	{
+		result[i] = s[i];
+		i++;
+	}
+	result[i] = '\0';
+	return (result);
+}
+
+char	*gnl_split(char **line, int i)
+{
+	char *result;
+	char *save;
+	int line_len;
+
+	line_len = gnl_strlen(*line);
+	result = gnl_strndup(*line, i + 1);
+	if (!result)
+		return (NULL);
+	if (line_len - 1 != i)
+	{
+		save = gnl_strndup(*line + i + 1, line_len - i - 1);
+		if (!save)
+		{
+			free(result);
+			return (NULL);
+		}
+	}
+	else
+		save = NULL;
+	free(*line);
+	*line = save;
+	return (result);
+}
